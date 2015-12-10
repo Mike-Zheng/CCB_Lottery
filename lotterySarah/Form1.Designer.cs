82ceebde549fe5e30a216e5dbfa72563a245018d
Mike-Zheng <liwenzheng.mike@gmail.com>
2015-12-10 13:00:23 +0800

    first commit

---
+++ lotterySarah/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace lotterySarah
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.load = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lottery = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.re = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // load
+            // 
+            this.load.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.load.Location = new System.Drawing.Point(398, 214);
+            this.load.Margin = new System.Windows.Forms.Padding(4);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(100, 72);
+            this.load.TabIndex = 0;
+            this.load.Text = "讀取名單";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 129);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(331, 365);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lottery
+            // 
+            this.lottery.Enabled = false;
+            this.lottery.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lottery.Location = new System.Drawing.Point(398, 309);
+            this.lottery.Margin = new System.Windows.Forms.Padding(4);
+            this.lottery.Name = "lottery";
+            this.lottery.Size = new System.Drawing.Size(100, 185);
+            this.lottery.TabIndex = 2;
+            this.lottery.Text = "抽籤";
+            this.lottery.UseVisualStyleBackColor = true;
+            this.lottery.Click += new System.EventHandler(this.lottery_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(42, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 81);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CCB小籤筒";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(431, 507);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "(c) 2015";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // re
+            // 
+            this.re.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.re.Location = new System.Drawing.Point(398, 129);
+            this.re.Name = "re";
+            this.re.Size = new System.Drawing.Size(100, 72);
+            this.re.TabIndex = 5;
+            this.re.Text = "重新開始";
+            this.re.UseVisualStyleBackColor = true;
+            this.re.Click += new System.EventHandler(this.re_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 533);
+            this.Controls.Add(this.re);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lottery);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.load);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "CCB小籤筒";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button lottery;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button re;
+    }
+}
+
---
+++ lotterySarah/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+
+namespace lotterySarah
+{
+   
+    public partial class Form1 : Form
+    {
+        public string[] list = new string[200];  //主畫格array
+        public int all=0;
+        public int cheat=0;
+        public int clickTimes = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+           
+            int i;
+            try
+            {
+               
+                System.IO.StreamReader reader = new System.IO.StreamReader(@"list.txt", System.Text.Encoding.Default);  
+                i = 0;
+                while (!reader.EndOfStream)
+                {
+
+                    string line = reader.ReadLine();
+                    if (!String.IsNullOrWhiteSpace(line))
+                    {
+                        string[] values = line.Split(',');
+                        if( values[0]!=null)list[i] = values[0];
+                       //list[i] = line.ToString();
+                       
+                    }
+                    i++;
+                    
+                }
+                all = i;
+
+                textBox1.Text ="add " + list[0]; 
+                for (int j = 1; j < all; j++)
+                  textBox1.Text = textBox1.Text + "\r\n" + "add " + list[j]; 
+            }
+            catch (FileNotFoundException ioEx)
+            {
+                Console.WriteLine(ioEx.Message);
+
+            }
+            
+            load.Enabled = false;
+            lottery.Enabled = true;
+
+        }
+
+        private void lottery_Click(object sender, EventArgs e)
+        {
+            lottery.Enabled = false;
+            Random r = new Random(DateTime.Now.Millisecond);
+           
+            if (clickTimes==0) textBox1.Text = "";
+            clickTimes++;
+                for (int j = 1; j <= 200; j++)
+                {
+                  
+                    Thread.Sleep(4);//每(1/1000秒)取得一次狀態,使用時須先導入using System.Threading;
+                    Application.DoEvents();//將狀態停止並顯示出來
+                    int rr=r.Next(0, all-1);
+                    label1.Text = list[rr].ToString();//範圍自行設定(大於等於1&&小於44-i)
+                }
+                if (cheat == 1)
+                {
+                    if (clickTimes == 1) label1.Text = "Kyle Tseng";
+                    else if (clickTimes == 2) label1.Text = "Eric tsao";
+                    else if (clickTimes == 3) label1.Text = "Lorraine Chen";
+                    else if (clickTimes == 4) label1.Text = "Arthur Lai";
+                    else if (clickTimes == 5) label1.Text = "Sarah Huang";
+                    else if (clickTimes == 6) label1.Text = "Dan Chang";
+                }
+                textBox1.Text = textBox1.Text+clickTimes + "." + label1.Text + "\r\n";
+                lottery.Enabled = true;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            cheat = 1;
+            label2.Text = "(c) 2015.";
+        }
+
+        private void re_Click(object sender, EventArgs e)
+        {
+            cheat = 0;
+            label2.Text = "(c) 2015";
+            all=0;
+            clickTimes = 0;
+            load.Enabled = true;
+            lottery.Enabled = false;
+            textBox1.Text = "";
+            label1.Text = "CCB小籤筒";
+        }
+
+      
+
+
+
+
+
+
+        
+    }
+}
+
+
